f800d90642c5524d8cb0083a7cb56be54f475615
Maikel Veen <veenmaikel@gmail.com>
2022-12-03 11:41:47 +0100

    feat: Add solution for day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::vec;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
@@ -37,10 +38,51 @@ fn ruck_sack(filename: &str) -> i32 {
     return sum;
 }
 
+fn ruck_sack_groups(filename: &str) -> i32 {
+    let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .collect();
+
+    let mut sum: i32 = 0;
+    let mut iter = 0;
+
+    let mut sets: Vec<HashSet<char>> = Vec::new();
+
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            if let Ok(rucksack) = line {
+                sets.push(rucksack.chars().collect::<HashSet<char>>());
+                iter += 1;
+
+                if iter == 3 {
+                    let intersection = sets.iter().skip(1).fold(sets[0].clone(), |acc, hs| {
+                        acc.intersection(hs).cloned().collect()
+                    });
+
+                    println!("{:?}", intersection);
+                    for c in intersection {
+                        sum += alphabet.iter().position(|&r| r == c).unwrap() as i32 + 1;
+                    }
+
+                    sets.clear();
+                    iter = 0;
+                }
+            }
+        }
+    }
+
+    return sum;
+}
+
 fn main() {
     let sum =
         ruck_sack("/Users/maikelveen/Projects/adventofcode2022/day3/src/inputs/input-day3.txt");
     println!("Sum: {}", sum);
+
+    let sum2 = ruck_sack_groups(
+        "/Users/maikelveen/Projects/adventofcode2022/day3/src/inputs/input-day3.txt",
+    );
+    println!("Sum: {}", sum2);
 }
 
 #[cfg(test)]
@@ -52,6 +94,15 @@ mod tests {
         assert_eq!(
             157,
             ruck_sack("/Users/maikelveen/Projects/adventofcode2022/day3/src/inputs/test.txt")
+        )
+    }
+    #[test]
+    fn test_sum_groups() {
+        assert_eq!(
+            70,
+            ruck_sack_groups(
+                "/Users/maikelveen/Projects/adventofcode2022/day3/src/inputs/test.txt"
+            )
         )
     }
 }
